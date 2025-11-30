cd0c6ad96bc6c270c4c6d52fb5f6157799aefc75
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2025-11-30 13:44:32 +0530

    Update hub.js

--- js/hub.js
+++ js/hub.js
@@ -1,1 +1,16 @@
+import {Progress} from "./progress.js";
+import {Engine} from "./engine.js";
 
+Engine.qsa(".map button").forEach(btn => {
+  const key = btn.dataset.key;
+
+  if (!Progress.allowed(key)) {
+    btn.classList.add("locked");
+    btn.disabled = true;
+  }
+
+  btn.onclick = async () => {
+    await Engine.fadeOut();
+    location.href = `pages/${key}.html`;
+  };
+});
