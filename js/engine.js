e6f96d65ff6ec1280edf01a4b9cdf455473c6529
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2025-11-30 13:42:10 +0530

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,10 @@
-
+export const Engine = {
+  qs: s => document.querySelector(s),
+  qsa: s => [...document.querySelectorAll(s)],
+  wait: ms => new Promise(r => setTimeout(r, ms)),
+  fadeOut: async () => {
+    document.body.classList.add("fade-out");
+    await Engine.wait(700);
+  },
+  fadeIn: () => document.body.classList.add("fade-in")
+};
