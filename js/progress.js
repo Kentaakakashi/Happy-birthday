3fd03fee6cf0d107ca4c62e6301627a247c84ee2
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2025-11-30 13:42:27 +0530

    Update progress.js

--- js/progress.js
+++ js/progress.js
@@ -1,1 +1,22 @@
+const steps = ["memory","photos","letters","us","happy","another"];
 
+export const Progress = {
+  get() {
+    return Number(sessionStorage.getItem("lv") || -1);
+  },
+  set(i) {
+    sessionStorage.setItem("lv", i);
+  },
+  unlock(key) {
+    const i = steps.indexOf(key);
+    if (i > this.get()) this.set(i);
+  },
+  allowed(key) {
+    return steps.indexOf(key) <= this.get() + 1;
+  },
+  reset() {
+    sessionStorage.clear();
+  }
+};
+
+window.onbeforeunload = () => Progress.reset();
